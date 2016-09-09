966e64ff88713c3422674698754c7b83e05a51e8
ant <akongx@gmail.com>
2016-09-09 16:52:21 -0700

    show dialog bubble thingy

--- app/detail/detail.js
+++ app/detail/detail.js
@@ -14,8 +14,21 @@ angular.module('chatApp.detail', ['ngRoute'])
     $scope.userId = parseInt($routeParams.uid, 10);
     var cid = parseInt($routeParams.id, 10);
     UserChatService.getChatById($scope.userId, cid).then(function(chat) {
+      console.log(chat.messages);
       $scope.participants = chat.participants;
-      $scope.messages = chat.messages;
+      $scope.messages = [];
+      // look at the next message
+      // if next message has the same sender_id as the current message, last is false
+      for(var i=0; i<chat.messages.length; i++) {
+        var msg = chat.messages[i];
+        msg.last = true;
+        var nextMsg = i+1 < chat.messages.length ? chat.messages[i+1] : null;
+        if(nextMsg && nextMsg.sender_id === msg.sender_id) {
+          msg.last = false;
+        }
+        $scope.messages.push(msg);
+      }
+
     });
   }
 ]);
